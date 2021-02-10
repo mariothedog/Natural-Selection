95344228ed9c1edf5e8808be4e65b180cbb6dc37
Mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-02-10 17:32:12 +0000

    Use private naming convention for RNGs

--- src/world/world.gd
+++ src/world/world.gd
@@ -5,8 +5,8 @@ const BASE_FOOD_SCENE := preload("res://food/base_food/base_food.tscn")
 
 var _grass_tile_positions := PoolVector2Array()
 
-var food_spawn_rng := Rand.get_new_random_generator()
-var mutation_rng := Rand.get_new_random_generator()
+var _food_spawn_rng := Rand.get_new_random_generator()
+var _mutation_rng := Rand.get_new_random_generator()
 
 onready var tilemap: TileMap = $TileMap
 onready var cell_size := tilemap.cell_size
@@ -32,7 +32,7 @@ func _ready() -> void:
 
 func _on_SpawnFood_timeout() -> void:
 	var food := BASE_FOOD_SCENE.instance()
-	food.position = Rand.choice(food_spawn_rng, _grass_tile_positions) + cell_size / 2
+	food.position = Rand.choice(_food_spawn_rng, _grass_tile_positions) + cell_size / 2
 	food_node.add_child(food)
 
 
@@ -45,6 +45,6 @@ func _on_Species_baby_wanted(parent: KinematicBody2D) -> void:
 
 
 func mutate(baby: KinematicBody2D, parent: KinematicBody2D) -> void:
-	var change := mutation_rng.randf_range(-50, 50)
+	var change := _mutation_rng.randf_range(-50, 50)
 	baby.phenotypes.speed = parent.phenotypes.speed + change
 	print("Parent Speed: %f  Baby Speed: %f" % [parent.phenotypes.speed, baby.phenotypes.speed])
