8ef6376bdd2b72a0d6ae2b9589eee98086daff66
Mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-02-11 01:22:52 +0000

    Fix time scale HUD bugs

--- src/ui/hud/time_scale.gd
+++ src/ui/hud/time_scale.gd
@@ -4,21 +4,19 @@ signal time_scale_changed(time_scale)
 
 var old_text := text
 
-var _max_time_scale = floor((pow(2, 31) - 1) / 60)
+var _max_time_scale := floor((pow(2, 31) - 1) / 60)
 
 
 func _on_TimeScale_text_changed(new_text: String) -> void:
-	if old_text == new_text:
+	var num := int(new_text)
+	if int(old_text) == num or num < 0:
+		text = old_text
 		return
 	
-	var num = int(new_text)
-	if (new_text.is_valid_integer() or new_text.empty()):
-		if num > _max_time_scale:
-			num = _max_time_scale
-		new_text = str(num)
-		if text != new_text:
-			text = new_text
-		old_text = new_text
-		emit_signal("time_scale_changed", num)
-	else:
-		text = old_text
+	num = int(min(num, _max_time_scale))
+	
+	new_text = str(num)
+	if text != new_text:
+		text = new_text
+	old_text = new_text
+	emit_signal("time_scale_changed", num)
