6ae20e7355b85a1ac2dea87296d18567e4f8fc86
Mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-02-11 01:12:09 +0000

    Add max value to time scale

--- src/ui/hud/time_scale.gd
+++ src/ui/hud/time_scale.gd
@@ -4,13 +4,21 @@ signal time_scale_changed(time_scale)
 
 var old_text := text
 
+var _max_time_scale = floor((pow(2, 31) - 1) / 60)
+
 
 func _on_TimeScale_text_changed(new_text: String) -> void:
 	if old_text == new_text:
 		return
-
-	if new_text.is_valid_integer() or new_text.empty():
+	
+	var num = int(new_text)
+	if (new_text.is_valid_integer() or new_text.empty()):
+		if num > _max_time_scale:
+			num = _max_time_scale
+		new_text = str(num)
+		if text != new_text:
+			text = new_text
 		old_text = new_text
-		emit_signal("time_scale_changed", int(new_text))
+		emit_signal("time_scale_changed", num)
 	else:
 		text = old_text
