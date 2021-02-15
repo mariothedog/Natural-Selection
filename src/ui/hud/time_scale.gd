7b47039a54f488415063a6fb932cecceabfd95e9
Mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-02-15 17:00:25 +0000

    Fix time scale LineEdit bugs

--- src/ui/hud/time_scale.gd
+++ src/ui/hud/time_scale.gd
@@ -2,21 +2,27 @@ extends LineEdit
 
 signal time_scale_changed(time_scale)
 
-var old_text := text
-
-var _max_time_scale := floor((pow(2, 31) - 1) / 60)
+var _old_text := text
+var _max_time_scale: int = floor((pow(2, 31) - 1) / 60)
 
 
 func _on_TimeScale_text_changed(new_text: String) -> void:
 	var num := int(new_text)
-	if int(old_text) == num or num < 0:
-		text = old_text
+	var old_num := int(_old_text)
+	if new_text.empty():
+		num = 0
+	elif num > _max_time_scale:
+		num = _max_time_scale
+		new_text = str(num)
+		var caret_position_temp := caret_position
+		text = new_text
+		caret_position = caret_position_temp
+	elif not new_text.is_valid_integer() or num < 0 or (old_num == num and not _old_text.empty()):
+		var caret_position_temp := caret_position
+		text = _old_text
+		caret_position = caret_position_temp
 		return
 
-	num = int(min(num, _max_time_scale))
-
-	new_text = str(num)
-	if text != new_text:
-		text = new_text
-	old_text = new_text
-	emit_signal("time_scale_changed", num)
+	if old_num != num:
+		emit_signal("time_scale_changed", num)
+	_old_text = new_text
